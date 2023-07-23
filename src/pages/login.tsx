5e3bbafd1ec5520b812378c2e9326087e5c7d033
shimizu-saffle <shimizu.saffle@gmail.com>
2023-07-23 22:44:43 +0900

    refactor: レイアウトコンポーネントを使って可読性を高める

--- src/pages/login.tsx
+++ src/pages/login.tsx
@@ -1,34 +1,29 @@
 import type { NextPage } from 'next'
 import Image from 'next/image'
-import { css } from '../../styled-system/css'
+import { Flex, VStack, styled } from '../../styled-system/jsx'
 import BrandButton from '../components/BrandButton'
 import googleLogo from '/public/google_logo.png'
 
 const Login: NextPage = () => {
   return (
     <>
-      <div
-        className={css({
-          display: 'flex',
-          flexDirection: 'column',
-          alignItems: 'center',
-          justifyContent: 'center',
-          height: '100vh',
-          gap: '1rem',
-        })}
-      >
-        <h1 className={css({ fontSize: '7xl' })}>Tabiyori</h1>
-        <div>{/* React Lottie でアニメーションを配置 */}</div>
-        <BrandButton
-          label={'Googleでログイン'}
-          brandLogo={
-            <Image src={googleLogo} alt="Google logo" width={50}></Image>
-          }
-          onClick={() => {}}
-          backgroundColor={'white'}
-          labelColor={'gray'}
-        />
-      </div>
+      <Flex align={'center'} justify={'center'} height={'100vh'}>
+        <VStack gap={'2rem'}>
+          <styled.h1 fontSize={'7xl'}>Tabiyori</styled.h1>
+          <div>{/* React Lottie でアニメーションを配置 */}</div>
+          <BrandButton
+            label={'Googleでログイン'}
+            brandLogo={
+              <Image src={googleLogo} alt="Google logo" width={50}></Image>
+            }
+            onClick={() => {
+              // TODO(shimizu-saffle): Googleログイン処理
+            }}
+            backgroundColor={'white'}
+            labelColor={'gray'}
+          />
+        </VStack>
+      </Flex>
     </>
   )
 }
