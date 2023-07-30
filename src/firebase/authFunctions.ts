f313c813d3fb8f96b7f5afd1a965c0ae21183a76
shimizu-saffle <shimizu.saffle@gmail.com>
2023-07-30 18:56:47 +0900

    refactor: lib/firebase へ移動

--- src/firebase/authFunctions.ts
+++
@@ -1,6 +0,0 @@
-import { GoogleAuthProvider, signInWithPopup } from 'firebase/auth'
-
-import { auth } from './clientApp'
-
-export const signInWithGoogle: unknown = async () =>
-  signInWithPopup(auth, new GoogleAuthProvider())
