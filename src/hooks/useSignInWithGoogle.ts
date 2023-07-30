aa21761dbcea1a9d5233fed6097154fb2a4837d6
shimizu-saffle <shimizu.saffle@gmail.com>
2023-07-30 19:01:14 +0900

    define: useSignInWithGoogle

---
+++ src/hooks/useSignInWithGoogle.ts
@@ -0,0 +1,33 @@
+import { GoogleAuthProvider, signInWithPopup } from 'firebase/auth'
+import nookies from 'nookies'
+import { useRecoilState } from 'recoil'
+import { appUserState } from '../atoms/userAtom'
+import { createUserWithFirebaseIdToken } from '../lib/api/authApi'
+import { auth } from '../lib/firebase/clientApp'
+
+export const useSignInWithGoogle = () => {
+  const provider = new GoogleAuthProvider()
+  const [, setCurrentUser] = useRecoilState(appUserState)
+
+  const signInWithGoogle = async () => {
+    try {
+      const result = await signInWithPopup(auth, provider)
+      const token = await result.user.getIdToken()
+      nookies.set(undefined, 'token', token)
+
+      if (result.user.displayName === null) {
+        // TODO(shimizu-saffle): エラーをユーザーに知らせるためのトーストを出す
+        return
+      }
+
+      const appUser = await createUserWithFirebaseIdToken(
+        result.user.displayName
+      )
+      setCurrentUser(appUser)
+    } catch (error) {
+      // TODO(shimizu-saffle): エラーをユーザーに知らせるためのトーストを出す
+      console.error(error)
+    }
+  }
+  return { signInWithGoogle }
+}
