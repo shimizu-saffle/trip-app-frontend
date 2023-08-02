affc087748b146c9973500baecfc7df013fbab25
shimizu-saffle <shimizu.saffle@gmail.com>
2023-08-02 22:04:42 +0900

    define: appUserFromJson

--- src/types/AppUser.ts
+++ src/types/AppUser.ts
@@ -1,5 +1,22 @@
+import { Json } from './ApiResponse'
+
 export interface AppUser {
   id: number
   name: string
   email: string
+}
+
+export function appUserFromJson(json: Json): AppUser {
+  if (
+    typeof json.id !== 'number' ||
+    typeof json.name !== 'string' ||
+    typeof json.email !== 'string'
+  ) {
+    throw new Error('Invalid JSON format for AppUser')
+  }
+  return {
+    id: json.id as number,
+    name: json.name as string,
+    email: json.email as string,
+  }
 }
